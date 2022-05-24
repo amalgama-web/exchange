61c72c584f4fee6955ee2d16b8f04fc0b641e65f
Yu Sh <amalgama43@gmail.com>
2022-05-24 14:09:54 +0300

    tests

---
+++ pages/imagetest.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    div
+        .l-container
+            h2 Image and picture tests
+            // img.test-image(src="@/assets/img/img01.jpg")
+            // что то странное не четко загружается именно по ширине
+            img.test-image(src="@/assets/img/img01.jpg"
+                srcset="@/assets/img/img01.jpg 100w"
+                sizes="100px"
+            )
+</template>
+
+<script>
+
+
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.test-image {
+    display: block;
+    width: 300px;
+    max-width: 100%;
+    height: auto;
+}
+</style>
