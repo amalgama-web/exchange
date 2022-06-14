53797451d9ce9f9c153cf8b51e58aa654b5b1057
Yu Sh <amalgama43@gmail.com>
2022-06-14 12:20:51 +0300

    tests

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
     .wrapper
         the-header
-        the-breadcrumbs
         nuxt
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,29 @@
 <template lang="pug">
     .l-container
 
-        .modal(v-if="modalIsOpen")
+        .modal(v-if="modalIsOpen1")
             .modal__inner
                 .modal__body
-                    button(@click="closeModal") close
+                    button(@click="toggleModal") close
                     br
+                    | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
                     | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
                     | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
                     input(type="text" v-model="text")
                     | enter text: {{text}}
 
-        p(@click="openModal")
+        .modal2(v-if="modalIsOpen2")
+            .modal2__body
+                button(@click="toggleModal") close
+                br
+                | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
+                | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
+                | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam iste libero magni molestias nam natus quisquam ratione reiciendis, reprehenderit, saepe sint totam voluptatem?
+                | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore ex, excepturi exercitationem fuga hic ipsam
+                input(type="text" v-model="text")
+                | enter text: {{text}}
+
+        p(@click="toggleModal")
             | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eaque eligendi excepturi exercitationem expedita impedit maiores modi molestiae nisi odit, placeat provident repudiandae saepe sit temporibus? Molestias necessitatibus odio veniam!
             | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eaque eligendi excepturi exercitationem expedita impedit maiores modi molestiae nisi odit, placeat provident repudiandae saepe sit temporibus? Molestias necessitatibus odio veniam!
             | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eaque eligendi excepturi exercitationem expedita impedit maiores modi molestiae nisi odit, placeat provident repudiandae saepe sit temporibus? Molestias necessitatibus odio veniam!
@@ -29,7 +41,8 @@
 
         data() {
             return {
-                modalIsOpen: false,
+                modalIsOpen1: false,
+                modalIsOpen2: false,
                 text: ''
             }
         },
@@ -53,12 +66,9 @@
         },
 
         methods: {
-            openModal() {
-                this.modalIsOpen = true;
+            toggleModal() {
+                this.modalIsOpen2 = !this.modalIsOpen2;
             },
-            closeModal() {
-                this.modalIsOpen = false;
-            }
         }
 
 
@@ -103,5 +113,50 @@
             }
         }
     }
-    
+
+    .modal2 {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+
+        left: 0;
+        top: 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &:before {
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            background-color: rgba(0,0,0,.4);
+        }
+
+
+        &__body {
+            position: relative;
+            padding: 30px;
+            background-color: #fff;
+            border-radius: 10px;
+            width: 450px;
+            max-width: calc(100% - 30px);
+
+
+            max-height: 100%;
+
+            overflow: auto;
+            input {
+                display: block;
+                width: 100%;
+                height: 40px;
+                padding: 0 10px;
+            }
+        }
+    }
+
+
 </style>
