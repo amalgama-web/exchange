11782b4a74a3e548f921d9552b0f7877e6216338
Yu Sh <amalgama43@gmail.com>
2022-07-22 13:32:03 +0300

    anchors test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
-    .l-container
-        h2 Страница для тестов
+    .l-container.test-section
+        h1 Главная
+        p
+            nuxt-link(:to="{ path: 'second', hash: 'id-02'}") Anchor to second page #2
+        p
+            nuxt-link(:to="{ path: 'second', hash: 'id-04'}") Anchor to second page #4
+        - var k = 0
+            while k++ < 5
+                div
+                    h1(id="id-0" + k) Заголовок #{k}
+                    - var n = 0;
+                    while n++ < 20
+                        | Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid blanditiis consectetur culpa delectus earum id illo incidunt nam nesciunt nobis quaerat qui quia quis quo rerum saepe unde, voluptate.
 
 </template>
 
@@ -19,8 +30,9 @@
 </script>
 
 <style lang="scss">
-    .info-section {
-        margin-bottom: 40px;
+.test-section {
+    h1 {
+        margin-top: 30px;
     }
-    
+}
 </style>
---
+++ pages/second.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    .l-container.test-section
+        h1 Вторая страница
+        p
+            nuxt-link(:to="{ path: 'test'}") Anchor to test page
+        - var k = 0
+                while k++ < 5
+                    div
+                        h1(id="id-0" + k) Заголовок #{k}
+                        - var n = 0;
+                        while n++ < 20
+                            | Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid blanditiis consectetur culpa delectus earum id illo incidunt nam nesciunt nobis quaerat qui quia quis quo rerum saepe unde, voluptate.
+
+</template>
+
+<script>
+
+export default {
+    meta: {
+        ruName: 'Главная'
+    },
+
+    computed: {
+    }
+
+
+}
+</script>
+
+<style lang="scss">
+.test-section {
+    h1 {
+        margin-top: 30px;
+    }
+}
+</style>
