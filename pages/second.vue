1047b5ab81c9526b0e4f60f711c307c8470f6404
Yu Sh <amalgama43@gmail.com>
2022-08-01 11:22:11 +0300

    ghost test tag filter

--- pages/second.vue
+++ pages/second.vue
@@ -12,17 +12,61 @@ export default {
         ruName: 'Главная'
     },
 
-    computed: {
-    },
+    computed: {},
 
     created() {
-        this.$axios.$get('http://localhost:2368/ghost/api/content/posts/?key=8527e622811a90d032c2c4c8fe', {
-            params: {
-                include: 'tags,authors',
-                fields: 'title,published_at,feature_image,primary_tag,slug',
-                filter: 'tag:-publications'
-            },
+        // tags slug
+        // this.$axios.$get('http://localhost:2368/ghost/api/content/tags/slug/news/', {
+        //     params: {
+        //         key: '8527e622811a90d032c2c4c8fe',
+        //         slug: 'news'
+        //     }
+        // });
+
+        // author info
+        // this.$axios.$get('http://localhost:2368/ghost/api/content/authors/slug/amalgama/', {
+        //     params: {
+        //         key: '8527e622811a90d032c2c4c8fe',
+        //         include: 'count.posts',
+        //     }
+        // });
+
+        // todo если нет фильтра publications то добавить в минус его
+
+        const params = {
+            key: '8527e622811a90d032c2c4c8fe',
+            include: 'tags,authors',
+            fields: 'title,published_at,feature_image,primary_tag,slug',
+            filter: 'tag:publications'
+        };
+
+
+        if (params.filter) {
+            if (!params.filter.includes('tag:publications')) {
+                console.log('filter exist and it not includes tag:publications');
+                // add excluding filter for publications
+                params.filter += '+tag:-publications'
+            }
+        } else {
+            params.filter = 'tag:-publications';
+        }
+
+        console.log('params');
+        console.log(params);
+
+        // posts not pubclications tag
+        this.$axios.$get('http://localhost:2368/ghost/api/content/posts/', {
+            params
         });
+
+        // all tags
+        // todo удалять publication tag
+        // this.$axios.$get('http://localhost:2368/ghost/api/content/tags/', {
+        //     params: {
+        //         key: '8527e622811a90d032c2c4c8fe',
+        //     }
+        // });
+
 
     }
 }
